388dd81fdf9bd69cc94b7cd0ba8f6aafebc6542d
Zijie Wu <noojeff@gmail.com>
2023-12-05 20:56:42 +0800

    Finish fetch logic

--- popup/popup.js
+++ popup/popup.js
@@ -1,25 +1,20 @@
 const ID_BTN_START_ANALYZE = "btnStartAnalyze";
+const ID_BTN_STOP_ANALYZE = "btnStopAnalyze";
 const ID_DIV_WRONG_URL = "errorWrongUrl";
 const URL_REPORT_PROBLEM_APPLE = "reportaproblem.apple.com";
 
 const btnStartAnalyze = document.getElementById(ID_BTN_START_ANALYZE);
+const btnStopAnalyze = document.getElementById(ID_BTN_STOP_ANALYZE);
 const divErrorWrongUrl = document.getElementById(ID_DIV_WRONG_URL);
 
-function onBtnStartAnalyze() {
-    // 1. check if the current tab is the target URL
-    chrome.tabs.query({ active: true, currentWindow: true },
-        (tabs) => {
-            var currentTab = tabs[0]; // there will be only one in this array
+let currentTab = null;
 
-            if (currentTab
-                && currentTab.hasOwnProperty("url")
-                && currentTab.url.includes(URL_REPORT_PROBLEM_APPLE)) {
-                // TODO: send message to service work to start fetching
-            } else {
-                // Show error on UI
-                divErrorWrongUrl.style.display = "block";
-            }
-        });
+function onBtnStartAnalyze() {
+    chrome.runtime.sendMessage({ message: "START", tabId: currentTab.id });
+}
+
+function onBtnStopAnalyze() {
+    chrome.runtime.sendMessage({ message: "STOP", tabId: currentTab.id });
 }
 
 function initUI() {
@@ -29,9 +24,28 @@ function initUI() {
 (function main() {
     console.log("popup.js main called");
 
+    // Get current Tab
+    chrome.tabs.query({ active: true, currentWindow: true },
+        (tabs) => {
+            var innerCurrentTab = tabs[0]; // there will be only one in this array
+
+            if (innerCurrentTab
+                && innerCurrentTab.hasOwnProperty("url")
+                && innerCurrentTab.url.includes(URL_REPORT_PROBLEM_APPLE)) {
+
+                currentTab = innerCurrentTab;
+            } else {
+                // Show error on UI
+                divErrorWrongUrl.style.display = "block";
+            }
+        });
+
     // Register button callback
     document.addEventListener('DOMContentLoaded', function () {
         btnStartAnalyze.addEventListener('click', onBtnStartAnalyze, false);
+    }, false)
+    document.addEventListener('DOMContentLoaded', function () {
+        btnStopAnalyze.addEventListener('click', onBtnStopAnalyze, false);
     }, false)
 
     // Init UI
--- scripts/background.js
+++ scripts/background.js
@@ -4,25 +4,26 @@ const URLS = ["*://reportaproblem.apple.com/api/purchase/search/*"];
 
 // GLOBAL VARIABLES
 let requestHistory = null;
+let mapTabIdFetchJobs = null;
 
 // Store HTTP request history
 // Key: tab ID
 class RequestHistory {
     constructor() {
         // Map<<tabId, requestId>, dsid>>
-        this.map_tabId_dsid = new Map();
+        this.mapTabIdDsid = new Map();
         // Map<<tabId, requestId>, dsid>>
-        this.map_tabId_headers = new Map();
+        this.mapTabIdHeaders = new Map();
 
         this.lastTabId = null;
         this.lastRequestId = null;
     }
 
-    to_key(tabId, requestId) {
+    toKey(tabId, requestId) {
         return tabId.toString() + "-" + requestId.toString();
     }
 
-    from_key(key) {
+    fromKey(key) {
         let arr = key.split("-");
         return {
             tabId: arr[0],
@@ -30,35 +31,35 @@ class RequestHistory {
         };
     }
 
-    record_before_request(details) {
+    recordBeforeRequest(details) {
         // Grab key
         const tabId = details.tabId;
         const requestId = details.requestId;
-        const key = this.to_key(tabId, requestId);
+        const key = this.toKey(tabId, requestId);
 
         // Grab dsid
         const raw = details.requestBody.raw[0].bytes;
         const decoder = new TextDecoder();
         const str = decoder.decode(raw);
         const obj = JSON.parse(str);
-    
+
         const dsid = obj["dsid"];
 
         // Set Map
-        this.map_tabId_dsid.set(key, dsid);
+        this.mapTabIdDsid.set(key, dsid);
         // console.log("map_tabId_dsid", this.map_tabId_dsid);
     }
 
-    record_send_headers(details) {
+    recordSendHeaders(details) {
         // Grab key
         const tabId = details.tabId;
         const requestId = details.requestId;
-        const key = this.to_key(tabId, requestId);
+        const key = this.toKey(tabId, requestId);
 
         const headers = details.requestHeaders;
 
         // Set Map
-        this.map_tabId_headers.set(key, headers);
+        this.mapTabIdHeaders.set(key, headers);
         // console.log("map_tabId_headers", this.map_tabId_headers);
 
         // Set the last tabId and requestId here
@@ -67,31 +68,142 @@ class RequestHistory {
     }
 }
 
+class FetchJob {
+    constructor(dsid, arr_headers) {
+        this.dsid = dsid;
+        this.headers = {};
+
+        // convert array of headers into dict
+        for (const each of arr_headers) {
+            let k = each["name"];
+            let v = each["value"];
+            this.headers[k] = v;
+        }
+
+        // 0: stopped
+        // 1: running
+        this.status = 0;
+    }
+
+    stop() {
+        this.status = 0;
+    }
+
+    // Fetch everything
+    async start() {
+        this.status = 1;
+
+        let result = await this.doFetch(null);
+
+        while (this.status === 1 && result.nextBatchId !== null) {
+            // Sleep
+            await new Promise(r => setTimeout(r, 400));
+
+            result = await this.doFetch(result.nextBatchId);
+        }
+    }
+
+    // Fetch once
+    async doFetch(batchId) {
+        const body = batchId === null ? {
+            dsid: this.dsid,
+        } : {
+            batchId,
+            dsid: this.dsid,
+        };
+
+        const res = await fetch("https://reportaproblem.apple.com/api/purchase/search", {
+            "headers": {
+                "accept-language": "en-US,en;q=0.9",
+                ...this.headers
+            },
+            "referrer": "https://reportaproblem.apple.com/?s=6",
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": JSON.stringify(body),
+            "method": "POST",
+            "mode": "cors",
+            "credentials": "include"
+        });
+
+        if (res.status !== 200) {
+            throw "Fetch failed";
+        }
+
+        const data = await res.json();
+        const nextBatchId = data.nextBatchId === "" ? null : data.nextBatchId;
+
+        return {
+            nextBatchId,
+            purchases: data.purchases
+        };
+    }
+}
 
 function registerListeners() {
     chrome.webRequest.onBeforeRequest.addListener((details) => {
-        requestHistory.record_before_request(details);
+        requestHistory.recordBeforeRequest(details);
     }, {
         urls: URLS
     }, ["requestBody", "extraHeaders"]);
 
 
     chrome.webRequest.onSendHeaders.addListener((details) => {
-        requestHistory.record_send_headers(details);
+        requestHistory.recordSendHeaders(details);
     }, {
         urls: URLS
     }, ["requestHeaders"]);
+
+    // Listen to messages from popup.js
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        console.log("Got message", request);
+
+        if (request.message === "START") {
+            const tabId = request.tabId;
+
+            startFetchJob(tabId);
+        } else if (request.message === "STOP") {
+            const tabId = request.tabId;
+
+            stopFetchJob(tabId);
+        }
+    });
 }
 
-// Unregister listeners when we start fetching
-function unregisterListeners() {
-    chrome.webRequest.onBeforeRequest.removeListener();
-    chrome.webRequest.onSendHeaders.removeListener();
+// // Unregister listeners when we start fetching
+// function unregisterListeners() {
+//     chrome.webRequest.onBeforeRequest.removeListener();
+//     chrome.webRequest.onSendHeaders.removeListener();
+// }
+
+function startFetchJob(tabId) {
+    if (requestHistory.lastTabId === null
+        || requestHistory.lastRequestId === null) {
+        throw "Refresh page and try again";
+    }
+
+    const key = requestHistory.toKey(requestHistory.lastTabId, requestHistory.lastRequestId);
+    const dsid = requestHistory.mapTabIdDsid.get(key);
+    const arr_headers = requestHistory.mapTabIdHeaders.get(key);
+
+    // Create a FetchJob for this tabId
+    if (mapTabIdFetchJobs.has(tabId)
+        && mapTabIdFetchJobs.get(tabId).status === 1) {
+        throw "Already started a Fetch Job for current tab";
+    }
+
+    const fetchJob = new FetchJob(dsid, arr_headers);
+    mapTabIdFetchJobs.set(tabId, fetchJob);
+    fetchJob.start();
+}
+
+function stopFetchJob(tabId) {
+    mapTabIdFetchJobs.has(tabId) && mapTabIdFetchJobs.get(tabId).stop();
 }
 
 // Reset all global variables
 function reset() {
     requestHistory = new RequestHistory();
+    mapTabIdFetchJobs = new Map();
 }
 
 (function main() {
