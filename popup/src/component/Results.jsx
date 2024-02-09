4bd898ab95b0f5899790d4b8678e0eda2bc19e7c
Zijie Wu <noojeff@gmail.com>
2024-02-08 17:43:04 -0800

    Add translation

--- popup/src/component/Results.jsx
+++ popup/src/component/Results.jsx
@@ -74,6 +74,7 @@ function ListAmount({ amounts, status }) {
 }
 
 function DownloadResult() {
+  const { t } = useTranslation();
   const state = useSelector((state) => state.state);
   const purchases = state.results.purchases || null;
 
@@ -99,7 +100,7 @@ function DownloadResult() {
       onClick={onClick}
       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
     >
-      Download your data as CSV
+      {t("result_download")}
     </button>
   );
 }
