2615f26159c8c6400b0f653f95bcf8eb4d48fce2
Zijie Wu <noojeff@gmail.com>
2024-02-08 17:20:03 -0800

    Switch to Vite bundler (#17)
    * Switch to Vite
    
    * Add prettier
    
    * Update App.jsx

---
+++ popup/src/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import store from "./store/store";
+
+import "./index.css";
+
+import "./i18n";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
